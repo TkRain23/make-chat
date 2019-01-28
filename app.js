6aa82de2abacef92026c8b5dab61d73e98fb65db
tkrain23 <tk.rain09@gmail.com>
2019-01-28 14:46:40 -0800

    [feat] setting up backend for incoming socket connections

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // app.js
-
 const express = require('express');
+
 const app = express();
 const path = require('path');
 
 // socket.io has to use the http server
 const server = require('http').Server(app);
 
+// socket.io
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('🔌 New user connected! 🔌');
+});
+
 // express view engine for pug
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
-})
+  res.render('index');
+});
 
 server.listen('3000', () => {
-    console.log('Server listening on Port 3000');
-})
+  console.log('Server listening on Port 3000');
+});
