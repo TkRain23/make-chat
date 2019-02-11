5093c70b71d4b1c30dfb3841048017b5b3ee53cd
tkrain23 <tk.rain09@gmail.com>
2019-02-10 21:10:45 -0800

    [new] adding chat feature

--- public/index.js
+++ public/index.js
@@ -1,13 +1,51 @@
-$(document).ready(() => {
-  // connect to the socket.io server
+//index.js
+$(document).ready(()=>{
   const socket = io.connect();
 
-  $('#createUserBtn').click((e) => {
-    let username = $('#usernameInput').val();
-    if (username.length > 0) {
-      // Emit to the server the new user
-      socket.emit('new user', username);
+  //Keep track of the current user
+  let currentUser;
+
+  $('#createUserBtn').click((e)=>{
+    e.preventDefault();
+    if($('#usernameInput').val().length > 0){
+      socket.emit('new user', $('#usernameInput').val());
+      // Save the current user when created
+      currentUser = $('#usernameInput').val();
       $('.usernameForm').remove();
+      // Have the main page visible
+      $('.mainContainer').css('display', 'flex');
     }
   });
-});
+
+  $('#sendChatBtn').click((e) => {
+    e.preventDefault();
+    let message = $('#chatInput').val();
+    // Make sure it's not empty
+    if(message.length > 0){
+      // Emit the message with the current user to the Server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      });
+      $('#chatInput').val("");;
+    }
+  });
+
+  //socket listeners
+  socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat`);
+    // Add the new user to the online users div
+    $('.usersOnline').append(`<div class="userOnline">${username}</div>`);
+  })
+
+  //Output the new message
+  socket.on('new message', (data) => {
+    $('.messageContainer').append(`
+      <div class="message">
+        <p class="messageUser">${data.sender}: </p>
+        <p class="messageText">${data.message}</p>
+      </div>
+    `);
+  })
+
+})
